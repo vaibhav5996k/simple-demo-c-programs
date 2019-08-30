bc2d53189b3b2f7a68355bdf9f924ad144f77d57
vaibhav <vibhukatkar96@gmail.com>
2019-08-30 12:04:23 +0530

    Natural no with reverse order

---
+++ revnat.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("\n Enter your natural no:");
+    scanf("%d",&n);
+
+    while (n>=1)
+    {
+        printf("%d ",n);
+        n--;
+    }
+    
+}
