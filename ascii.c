53c625309cbffa5caa29b7caac08909420c72a6e
vaibhav <vibhukatkar96@gmail.com>
2019-08-30 15:11:44 +0530

    ascii value of any word

---
+++ ascii.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ 
+ int main(){
+     
+         char a;
+
+         printf("Enter any charecter:\n");
+         scanf("%c",&a);
+
+        printf("Asci value of your charecter %c is %d",a,a);
+
+     return 0;
+ }
